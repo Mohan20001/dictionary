67cdda0c9dac8f0e22edc62ef03691a3d7cfd5ea
mohan putta <puttamohan2001@gmail.com>
2021-10-01 15:50:50 +0530

    master

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function getDATA(url="https://api.dictionaryapi.dev/api/v2/entries/en/mocking") {
-    fetch(url).then(data => data.json()).then(Response=>document.body.innerText=Response[0].word);
+    fetch(url).then(data => data.json()).then(Response=>console.log(Response[0].word));
 }
 
 getDATA();
