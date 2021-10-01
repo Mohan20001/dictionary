97acac69c168f3ccfbe625a19797d485c75b0763
mohan putta <puttamohan2001@gmail.com>
2021-10-01 17:28:09 +0530

    meaning update

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-function getDATA(url="https://api.dictionaryapi.dev/api/v2/entries/en/mocking") {
-    fetch(url).then(data => data.json()).then(Response=>console.log(Response[0].word));
-}
+let txt=document.querySelector('input[type="search"]');
+let btn=document.getElementById('btn-search');
 
-getDATA();
+btn.addEventListener('click',()=>{
+   getDATA(txt.value);
+});
+
+
+function getDATA(word="mocking") {
+    url=`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    fetch(url).then(data => data.json()).then(Response=>{
+        alert(Response[0].meanings[0].definitions[0].definition);
+        console.log(Response);
+    });
+}
 ///comment added
