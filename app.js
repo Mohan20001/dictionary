86ba13cb5272aba831280dd13c12b08a601114cf
mohan putta <puttamohan2001@gmail.com>
2021-10-04 17:05:41 +0530

    some responsive content added

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 let txt=document.querySelector('input[type="search"]');
 let btn=document.getElementById('btn-search');
 var h=document.getElementById('w');
+let speaker=document.getElementById('speaker');
+let mean=document.getElementById('meaning');
+let pron=document.getElementById('pron');
+
+speaker.addEventListener('click',()=>{
+    ad.play();
+});
+
 
 btn.addEventListener('click',()=>{
    getDATA(txt.value);
@@ -11,8 +19,9 @@ function getDATA(word="mocking") {
     url=`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     fetch(url).then(data => data.json()).then(Response=>{
         alert(Response[0].meanings[0].definitions[0].definition);
+        h.innerText=Response[0].word;
         console.log(Response);
-        h.innerHTML=Response[0].meanings[0].definitions[0].definition;
+        mean.innerHTML=Response[0].meanings[0].definitions[0].definition;
     });
 }
 ///comment added
